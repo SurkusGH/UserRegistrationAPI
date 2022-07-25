284068176156baee1effec2f1bdad99aeb49bcd1
SurkusGH <v.surkus@me.com>
2022-07-25 15:21:52 +0300

    +sqlConnection +firstMigration

---
+++ UserRegistrationAPI/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserRegistrationAPI.Models;
+
+namespace UserRegistrationAPI
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<InfoSheet> InfoSheets { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
---
+++ UserRegistrationAPI/Migrations/20220725122007_DatabaseCreation.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserRegistrationAPI.Migrations
+{
+    public partial class DatabaseCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    House = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apartanemt = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InfoSheets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalNumber = table.Column<decimal>(type: "decimal(20,0)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    AddressId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfoSheets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InfoSheets_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InfoSheetId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_InfoSheets_InfoSheetId",
+                        column: x => x.InfoSheetId,
+                        principalTable: "InfoSheets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfoSheets_AddressId",
+                table: "InfoSheets",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_InfoSheetId",
+                table: "Users",
+                column: "InfoSheetId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "InfoSheets");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
