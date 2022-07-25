023af87d42e5622244ed657b7a6cf4b6b898da8a
SurkusGH <v.surkus@me.com>
2022-07-25 16:32:34 +0300

    + IUnitOfWork interface

---
+++ UserRegistrationAPI/Repositories/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using UserRegistrationAPI.Models;
+
+namespace UserRegistrationAPI.Repositories.IRepository
+{
+    public interface IUnitOfWork
+    {
+        public interface IUnitOfWork : IDisposable
+        {
+            IGenericRepository<User> Users { get; }
+            IGenericRepository<InfoSheet> InfoSheets { get; }
+            IGenericRepository<Address> Addresses { get; }
+            Task Save();
+        }
+    }
+}
